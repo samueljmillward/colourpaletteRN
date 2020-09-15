135311a2d6bdf3bf39552dd9ee351a71bc28a086
samueljmillward <samuel.j.pearce@hotmail.com>
2020-09-15 23:25:54 +0100

    Coloured boxes(?)

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Here are some boxes of different colours:</Text>
+        <View style={[styles.box, styles.cyan]}>
+          <Text style={styles.box}>Cyan: #2aa198</Text>
+        </View>
+        <View style={[styles.box, styles.blue]}>
+          <Text style={styles.box}>Blue: #2aa198</Text>
+        </View>
+        <View style={[styles.box, styles.magenta]}>
+          <Text style={styles.box}>Magenta: #FF00FF</Text>
+        </View>
+        <View style={[styles.box, styles.orange]}>
+          <Text style={styles.box}>Orange: #FF00FF</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingTop: 40,
+  },
+  text: {
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
+  box: {
+    padding: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#FF00FF',
+  },
+  orange: {
+    backgroundColor: 'orange',
+  },
+  boxText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
