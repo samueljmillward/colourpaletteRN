61efb561fae26980b7476e9f01cad2e59aaf2f2a
samueljmillward <samuel.j.pearce@hotmail.com>
2020-09-21 15:34:43 +0100

    Submit form

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Alert } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const ColorPaletteModal = () => {
+    const [name, setName] = useState('');
+
+    const handleSubmit = useCallback(() => {
+        if (!name) {
+            Alert.alert('Please enter a palette name')
+        }
+    }, [name]);
+
     return (
-        <View>
-            <Text>Add Color Palette</Text>
+        <View style={styles.container}>
+            <Text style={styles.name}> Palette Name:</Text>
+            <TextInput
+                style={styles.input}
+                value={name}
+                onChangeText={setName} />
+            <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+                <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    input: {
+        borderColor: 'grey',
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 50,
+    },
+    container: {
+        padding: 10,
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    button: {
+        height: 40,
+        backgroundColor: 'teal',
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    name: {
+        marginBottom: 10,
+    }
+});
 
 export default ColorPaletteModal;
